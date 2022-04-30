512d963772518a137873e3dd58306798be79afb9
Joey Cos <joeycosentino24@gmail.com>
2022-04-30 15:23:53 -0400

    added post requests and validation

--- server.js
+++ server.js
@@ -2,6 +2,14 @@ const express = require('express');
 // const port should be before const app, not exactly sure why but thats what module says
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+const fs = require('fs');
+const path = require('path');
+//parse income string or array data
+app.use(express.urlencoded({ extended: true }));
+// parse incoming JSON data
+app.use(express.json());
+
 // this creates a route that the front-end can request data from
 const { animals } = require('./data/animals');
 
@@ -51,6 +59,33 @@ function findById(id, animalsArray) {
     return result;
 }
 
+function createNewAnimal(body, animalsArray) {
+    const animal = body;
+    animalsArray.push(animal);
+    fs.writeFileSync(
+        path.join(__dirname, './data/animals.json'),
+        JSON.stringify({ animals: animalsArray}, null, 2)
+    );
+    
+    return animal;
+}
+
+function validateAnimal(animal) {
+    if (!animal.name || typeof animal.name !== 'string') {
+        return false;
+    }
+    if (!animal.species || typeof animal.species !== 'string') {
+        return false;
+    }
+    if (!animal.diet || typeof animal.diet !== 'string') {
+        return false;
+    }
+    if (!animal.personalityTraits || !Array.isArray(animal.personalityTraits)) {
+        return false;
+    }
+    return true;
+}
+
 // this adds the route. the added route should be typed before app.listen()
 app.get('/api/animals', (req, res) => {
     let results = animals;
@@ -70,6 +105,21 @@ app.get('/api/animals/:id', (req, res) => {
         
 });
 
+app.post('/api/animals', (req, res) => {
+    // set id based on what the next index of the array will be
+    req.body.id = animals.length.toString();
+
+    // if any data in req.body is incorrect, send 400 error back
+    if (!validateAnimal(req.body)) {
+        res.status(400).send('The animal is not properly formatted.');
+    } else {
+        const animal = createNewAnimal(req.body, animals);
+        res.json(animal);
+    }
+});
+
 app.listen(PORT, () => {
     console.log('API server now on port 3001!');
 });
+
+// LEFT OFF 11.2.1 INTRODUCTION
